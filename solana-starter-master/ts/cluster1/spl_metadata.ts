a636169b0ef9bb42c23a42c1eaef7950c8c48f3f
ThotSploit <119318084+ThottySploity@users.noreply.github.com>
2025-01-15 23:32:52 +0100

    Wednesday class

--- solana-starter-master/ts/cluster1/spl_metadata.ts
+++ solana-starter-master/ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../dev-wallet.json"
+import wallet from "/home/thotty/Turbin3-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("ABxF4BTGQguZG82ALJiNf2jp2bG5d5bxpDACYy8bEFku")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -18,31 +18,41 @@ const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
+// Token: 2FGv3nCwRTMX9eSDyhejwxGvnTa1ktijfDVggs2PGnJHWAwNBeYLu7HTYV6R7gHyXcZKBS846V2b6M8jb5XnkJu
+
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint, mintAuthority:signer
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "turbin3_Q1_2025_Thotty",
+            symbol: "thot",
+            uri: "https://arweave.net",
+            sellerFeeBasisPoints: 1,
+            creators: null,
+            collection: null,
+            uses: null,
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data: data,
+            isMutable: true,
+            collectionDetails: null,
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
--- solana-starter-master/ts/cluster1/spl_transfer.ts
+++ solana-starter-master/ts/cluster1/spl_transfer.ts
@@ -1,6 +1,6 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
-import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import wallet from "/home/thotty/Turbin3-wallet.json"
+import { getOrCreateAssociatedTokenAccount, transfer, transferFeeLayout } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,18 +10,26 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("ABxF4BTGQguZG82ALJiNf2jp2bG5d5bxpDACYy8bEFku");
 
-// Recipient address
-const to = new PublicKey("<receiver address>");
+// Recipient public address
+const to = new PublicKey("2CquYcQoBGv8MiiMfP3Lgut79oLCtDbCTrB6fnQm1WeG"); // Apaar Saini
+
+// Tx/Signature = 5U6DAWCXPrArkSTZtR7TBJbRWtvZ8Hm4haiSJBUdjm7CppfsZcE7LxJBcRJ9C5xFCbkdugUo9cgHazqst5yt3Xu2
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromWallet = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const toWallet = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const signature = await transfer(connection, keypair, fromWallet.address, toWallet.address, keypair, 69);
+
+        console.log(`Signature; ${signature}`);
+
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }

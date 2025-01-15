a55ea97b6ea4ac8d370669a6200364856971cfb0
ThotSploit <119318084+ThottySploity@users.noreply.github.com>
2025-01-15 19:37:02 +0100

    Minting ATA

---
+++ solana-starter-master/ts/cluster1/spl_mint.ts
@@ -0,0 +1,33 @@
+import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import wallet from "/home/thotty/Turbin3-wallet.json"
+
+// Import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+const token_decimals = 1_000_000n;
+
+// Mint address
+const mint = new PublicKey("ABxF4BTGQguZG82ALJiNf2jp2bG5d5bxpDACYy8bEFku");
+
+// Your ata is: H1ueocg5Ds3DD7hN7EqFpUSPAeZMnK7DS7sxwkuUvbzW
+// Your mint txid: 4qdj176vRRMdRVk3b74XcPAx9p2zDyoY2i8gwf9pVBYZfeEJ2LtEsqa6B63zMWZNFYvQsUNQkEANYvjATKmgrFXt
+
+(async () => {
+    try {
+        // ATA == Associated Token Account
+        // Create an ATA
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+
+        // Mint to ATA
+        const mintTx = await mintTo(connection, keypair, mint, ata.address, keypair.publicKey, 1n * token_decimals);
+        console.log(`Your mint txid: ${mintTx}`);
+    } catch(error) {
+        console.log(`Oops, something went wrong: ${error}`)
+    }
+})()
